f98dd509572b8c4e98f3d863febb9d3a9701c2e4
Jeries Nasrawi <jnasrawi@asaltech.com>
2019-06-26 17:00:25 +0300

    minor backened commenting changes

--- SignUp.php
+++ SignUp.php
@@ -9,7 +9,6 @@
         //creating string and create request
         $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
         //decoding from json
-        echo $recaptcha;
 
         $recaptcha = json_decode($recaptcha);
         if ($recaptcha->score >= 0.5) {
@@ -20,27 +19,27 @@
         //connecting to database
          //connecting to database
         
-    //      $servername = "localhost";
-    //      $username = "root";
-    //      $password = "";
-    //      $dbname = "task1";
+         $servername = "localhost";
+         $username = "root";
+         $password = "";
+         $dbname = "task1";
  
-    //      $conn = new mysqli($servername, $username, $password, $dbname);
-    //      if ($conn->connect_error) {
-    //         die("Connection failed: " . $conn->connect_error);
-    //     } 
-    //     $sql = "INSERT INTO user (email, password,firstName,lastName,age,address)
-    //     VALUES ('".$_POST['SignUpEmail']."', '".$_POST['SignUpPassword']."', '".$_POST['SignUpFirstName']."', '".$_POST['SignUpLastName']."', " .$_POST['SignUpAge'].",'". $_POST['SignUpAddress']."')";
+         $conn = new mysqli($servername, $username, $password, $dbname);
+         if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        $sql = "INSERT INTO user (email, password,firstName,lastName,age,address)
+        VALUES ('".$_POST['SignUpEmail']."', '".$_POST['SignUpPassword']."', '".$_POST['SignUpFirstName']."', '".$_POST['SignUpLastName']."', " .$_POST['SignUpAge'].",'". $_POST['SignUpAddress']."')";
 
-    //  //execute query
+     //execute query
      
-    // if ($conn->query($sql) === TRUE) {
-    // echo "success";
-    // } else {
-    // echo "404";
-    // }
+    if ($conn->query($sql) === TRUE) {
+    echo "success";
+    } else {
+    echo "404";
+    }
 
-    // $conn->close();
+    $conn->close();
 
     }else{
         echo "bot";
